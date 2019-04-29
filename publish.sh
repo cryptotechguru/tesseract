63ff6d12349ad77101f781290a4cc424667342e7
David McFadzean <davidmc@gmail.com>
2019-04-29 15:43:10 -0400

    Fix for exe path

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ BUILD_TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 echo "--------------------- Publishing Core Binaries ---------------------"
 git clone https://$GH_USR:$GH_TKN@github.com/cryptotechguru/windows-binaries.git
 cd windows-binaries
-cp -f /home/travis/build/cryptotechguru/tesseract-core/build/equibit-x86_64-w64-mingw32/src/*.exe node
+cp -f /home/travis/build/cryptotechguru/tesseract-core/build/tesseract-x86_64-w64-mingw32/src/*.exe node
 git add *
 git commit -m "Tesseract binaries updated on ${BUILD_TIMESTAMP}"
 git push origin master
