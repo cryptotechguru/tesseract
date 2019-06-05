e9ceec2d9f2b330e36673f7ec3106c0cfca2b033
David McFadzean <davidmc@gmail.com>
2019-06-05 16:52:43 -0400

    Fix rpc_blockchain

--- eqbtest/functional/rpc_blockchain.py
+++ eqbtest/functional/rpc_blockchain.py
@@ -133,7 +133,7 @@ class BlockchainTest(BitcoinTestFramework):
         node = self.nodes[0]
         res = node.gettxoutsetinfo()
 
-        assert_equal(res['total_amount'], Decimal('2760.78479039'))
+        assert_equal(res['total_amount'], Decimal('1043229.77802551'))
         assert_equal(res['transactions'], 200)
         assert_equal(res['height'], 200)
         assert_equal(res['txouts'], 200)
